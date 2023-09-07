606c955e69d794b6e16116b6d53b170dbc2d598a
Marcel Kirsch <marcel.kirsch@amc-gmbh.com>
2023-09-07 13:42:04 +0200

    refactoring verarbeitung des Strings

--- src/OrdinalNumbers.java
+++ src/OrdinalNumbers.java
@@ -7,107 +7,116 @@ public class OrdinalNumbers {
 
     public static String zahlenBuilder(int number) {
         String result = "";
-        int einserStelle = number % 10;                 //58 % 10       -> 8
-        int hundertStelle = number / 100;               //205 / 100     -> 2
-        int hundertZehnerStelle = number / 10 % 10;     //123 / 10 % 10 -> 2
-
-        StringBuilder sb = new StringBuilder();
-
-        String hundert = "hundert";
 
         /* Zahlen bis 12 */
         if (number <= 12) return einstelligeZahlen[number];
 
         /* HUNDERTER */
-        if (hundertStelle > 0) {
-            char atHundret = String.valueOf(number).charAt(0);
-            char atTen = String.valueOf(number).charAt(1);
-            char atOne = String.valueOf(number).charAt(2);
+        if (String.valueOf(number).length() == 3)
+            return processHundret(number);
+        if (String.valueOf(number).length() < 3)
+            return processTens(number);
 
-            //Ein Hundert ster, Zwei Hundert ster, Drei Hundert ster, Fünf Hundert ster
-            String einserStelleLowercase = ordinalZahlen[einserStelle].toLowerCase();   //zweiter, vierter
+        return result;
+    }
 
-            //200, 500, 600
-            //Volle Hunderter
-            if (atHundret != '0' && atTen == '0' && atOne == '0') {
-                System.out.println("A");
-                result = sb.append(einstelligeZahlen[hundertStelle])
-                        .append(hundert)
-                        .append("ster").toString();
 
-                //450, 120, 690, 410
-                //Alles ohne Einser
-            } else if (atHundret != '0' && atTen != '0' && atOne == '0') {
-                System.out.println("B");
-                result = sb.append(einstelligeZahlen[hundertStelle])
-                        .append(hundert)
-                        .append(zehnerZahlen[hundertZehnerStelle].toLowerCase())
-                        .append("ster").toString();
+    private static String processHundret (int number) {
+        StringBuilder sb = new StringBuilder();
+        String result = "";
+        String hundert = "hundert";
 
-                //103, 506, 409, 504
-                //Alles ohne Zehner
-            } else if (atHundret != '0' && atTen == '0' && atOne != '0') {
-                System.out.println("C");
-                result = sb.append(einstelligeZahlen[hundertStelle])
-                        .append(hundert).append("und")
-                        .append(einserStelleLowercase).toString();
+        int hundertStelle = number / 100;               //205 / 100     -> 2
+        int hundertZehnerStelle = number / 10 % 10;     //123 / 10 % 10 -> 2
+        int einserStelle = number % 10;                 //58 % 10       -> 8
 
-                //Drei Hundert Zwölf ter, Zwei Hundert Elf ter
-                //Elf und Zwölf Sonderfälle
-            } else if (number % 100 <= 12 && number % 100 >= 11) {
-                System.out.println("D");
-                int dezimalZahl = number % 100;
-                result = sb.append(einstelligeZahlen[hundertStelle])
-                        .append(hundert)
-                        .append(ordinalZahlen[dezimalZahl].toLowerCase()).toString();
+        char atHundret = String.valueOf(number).charAt(0);
+        char atTen = String.valueOf(number).charAt(1);
+        char atOne = String.valueOf(number).charAt(2);
 
-                // Hundert Drei Zehn ter, Hundert Fünf Zehn ter, Hundert Sieb Zehn ter
-                //Zahlen von Hunder Drei Zehn ter bis Neun Zehn ter
-            } else if (number % 100 > 12 && number % 100 < 20) {
-                System.out.println("E");
-                result = sb.append(einstelligeZahlen[hundertStelle])
-                        .append(hundert)
-                        .append(einstelligeZahlen[einserStelle].toLowerCase())
-                        .append(zehnerZahlen[hundertZehnerStelle].toLowerCase())
-                        .append("ter").toString();
+        //Ein Hundert ster, Zwei Hundert ster, Drei Hundert ster, Fünf Hundert ster
+        String einserStelleLowercase = ordinalZahlen[einserStelle].toLowerCase();   //zweiter, vierter
 
-                //Drei Hundert Zwei und Dreißig ster, Vier Hundert Sechs und Vierzig ster
-                //323, 674, 215
-            } else {
-                System.out.println("E");
-                result = sb.append(einstelligeZahlen[hundertStelle])
-                        .append(hundert)
-                        .append(einstelligeZahlen[einserStelle].toLowerCase())
-                        .append("und")
-                        .append(zehnerZahlen[hundertZehnerStelle].toLowerCase())
-                        .append("ter").toString();
-            }
+        //200, 500, 600
+        //Volle Hunderter
+        if (atHundret != '0' && atTen == '0' && atOne == '0') {
+            result = sb.append(einstelligeZahlen[hundertStelle])
+                    .append(hundert)
+                    .append("ster").toString();
 
-            /* ZEHNER */
+            //450, 120, 690, 410
+            //Alles ohne Einser
+        } else if (atHundret != '0' && atTen != '0' && atOne == '0') {
+            result = sb.append(einstelligeZahlen[hundertStelle])
+                    .append(hundert)
+                    .append(zehnerZahlen[hundertZehnerStelle].toLowerCase())
+                    .append("ster").toString();
+
+            //103, 506, 409, 504
+            //Alles ohne Zehner
+        } else if (atHundret != '0' && atTen == '0' && atOne != '0') {
+            result = sb.append(einstelligeZahlen[hundertStelle])
+                    .append(hundert).append("und")
+                    .append(einserStelleLowercase).toString();
+
+            //Drei Hundert Zwölf ter, Zwei Hundert Elf ter
+            //Elf und Zwölf Sonderfälle
+        } else if (number % 100 <= 12 && number % 100 >= 11) {
+            int dezimalZahl = number % 100;
+            result = sb.append(einstelligeZahlen[hundertStelle])
+                    .append(hundert)
+                    .append(ordinalZahlen[dezimalZahl].toLowerCase()).toString();
+
+            // Hundert Drei Zehn ter, Hundert Fünf Zehn ter, Hundert Sieb Zehn ter
+            //Zahlen von Hunder Drei Zehn ter bis Neun Zehn ter
+        } else if (number % 100 > 12 && number % 100 < 20) {
+            result = sb.append(einstelligeZahlen[hundertStelle])
+                    .append(hundert)
+                    .append(einstelligeZahlen[einserStelle].toLowerCase())
+                    .append(zehnerZahlen[hundertZehnerStelle].toLowerCase())
+                    .append("ter").toString();
+
+            //Drei Hundert Zwei und Dreißig ster, Vier Hundert Sechs und Vierzig ster
+            //323, 674, 215
         } else {
-            int zehnerStelle = number / 10;    //34 / 10 -> 3
-            String zehnerLowercase = zehnerZahlen[zehnerStelle].toLowerCase(); //zehnter, dreißigster
+            result = sb.append(einstelligeZahlen[hundertStelle])
+                    .append(hundert)
+                    .append(einstelligeZahlen[einserStelle].toLowerCase())
+                    .append("und")
+                    .append(zehnerZahlen[hundertZehnerStelle].toLowerCase())
+                    .append("ter").toString();
+        }
+        return result;
+    }
 
-            //bis 20
-            if (number < 20) {
+    private static String processTens(int number) {
+        String result = "";
+        StringBuilder sb = new StringBuilder();
+
+        int zehnerStelle = number / 10;    //34 / 10 -> 3
+        int einserStelle = number % 10;    //58 % 10 -> 8
+
+        String zehnerLowercase = zehnerZahlen[zehnerStelle].toLowerCase(); //zehnter, dreißigster
+
+        //bis 20
+        if (number < 20) {
+            result = sb.append(einstelligeZahlen[einserStelle])
+                    .append(zehnerLowercase)
+                    .append("ter").toString();
+
+            //alles ab 20
+        } else {
+            //volle zehner (30, 40, 50, 60)
+            if (number % 10 == 0) {
                 result = sb.append(einstelligeZahlen[einserStelle])
-                        .append(zehnerLowercase)
-                        .append("ter").toString();
-
-                //alles ab 20
+                        .append(zehnerZahlen[zehnerStelle])
+                        .append("ster").toString();
             } else {
-                //volle zehner (30, 40, 50, 60)
-                if (number % 10 == 0) {
-                    result = sb.append(einstelligeZahlen[einserStelle])
-                            .append(zehnerZahlen[zehnerStelle])
-                            .append("ster").toString();
-                } else {
-                    //zehner mit zahl (32, 45, 58, 93)
-                    result = sb.append(einstelligeZahlen[einserStelle])
-                            .append("und")
-                            .append(zehnerLowercase)
-                            .append("ster").toString();
-                }
+                //zehner mit zahl (32, 45, 58, 93)
+                result = sb.append(einstelligeZahlen[einserStelle])
+                        .append("und")
+                        .append(zehnerLowercase)
+                        .append("ster").toString();
             }
         }
         return result;
